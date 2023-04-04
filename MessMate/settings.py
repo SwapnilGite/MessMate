dd5f42f4c392486aad5219ac93d1cc614ecbd879
SwapnilGite <gitesmait@gmail.com>
2023-04-04 23:56:29 +0530

    super_user

--- MessMate/settings.py
+++ MessMate/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'messdatabase',
         'USER': 'root',
-        'PASSWORD' : 'Devgiri@20',
+        'PASSWORD' : 'root',
         'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS': { 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" }
