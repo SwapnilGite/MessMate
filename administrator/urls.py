d75a02eed7f700a11f2fc86b6bb227549dc24492
Sumit Girnar <sumitgirnar09@gmail.com>
2023-04-04 23:50:21 +0530

    minute changes

--- administrator/urls.py
+++ administrator/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('AdminLogin/',views.AdminLogin,name='AdminLogin'),
+    # templates\adminstrator\adminlogin.html
     # path('studentLogin/',include('student.urls')),
     # path('FacultyLogin/',include('faculty.urls')),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -5,29 +5,29 @@ from django.shortcuts import render
 from django.contrib.auth  import authenticate,  login, logout
 
 def home(request):
-    return render(request,'adminstrator/adminhome.html')
+    return render(request,'administrator/adminhome.html')
 
 def AdminLogin(request):
     # return HttpResponse("Admin Login Entered")
-    # return render(request,'admin/adminLogin.html')
+    return render(request,'administrator/adminlogin.html')
     
-    if request.method=="POST":
+    # if request.method=="POST":
         
-        loginusername=request.POST['loginusername']
-        loginpassword=request.POST['loginpassword']
-        loginemail=request.POST['loginemail']
+    #     loginusername=request.POST['loginusername']
+    #     loginpassword=request.POST['loginpassword']
+    #     loginemail=request.POST['loginemail']
         
-        # print("loginusername : ",loginusername)
-        # print("loginpassword : ",loginpassword)
+    #     # print("loginusername : ",loginusername)
+    #     # print("loginpassword : ",loginpassword)
         
-        user=authenticate(username= loginusername, password= loginpassword)
-        if user is not None and request.user.is_superuser:
-            login(request, user)
-            return HttpResponse("Superuser Logged In")
-            # return render(request,"admin/adminLogin.html")
-        else:
-            # loginstatus={"valid":0}
-            # return render(request,"student/home.html",loginstatus)
-            return HttpResponse("You are not allowed to visit this page")
+    #     user=authenticate(username= loginusername, password= loginpassword)
+    #     if user is not None and request.user.is_superuser:
+    #         login(request, user)
+    #         return HttpResponse("Superuser Logged In")
+    #         # return render(request,"admin/adminLogin.html")
+    #     else:
+    #         # loginstatus={"valid":0}
+    #         # return render(request,"student/home.html",loginstatus)
+    #         return HttpResponse("You are not allowed to visit this page")
     
-    return render(request,"administrator/adminlogin.html")
+    # return render(request,"administrator/adminlogin.html")
